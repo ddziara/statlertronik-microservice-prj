fb24ae04c0ab394757aa772721112461a3f12e3c
Dariusz Dziara <ddziara@poczta.onet.pl>
2022-07-16 17:45:13 +0200

    Port changed to either dynamic or 3000; package.json modified

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+const port = process.env.PORT || 3000;
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });

9d1bb45612601f81f6ad77659811439bd8361957
Dariusz Dziara <ddziara@poczta.onet.pl>
2022-07-17 14:40:25 +0200

    Pages added, repositories renamed

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + "/views/timestamp.html");
+});
+
+app.get("/requestHeaderParser", function (req, res) {
+  res.sendFile(__dirname + "/views/requestHeaderParser.html");
+});
+
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });

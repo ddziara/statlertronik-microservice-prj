94ccc4a33eb618d662fb33c8484a797f4c12cd04
Dariusz Dziara <ddziara@poczta.onet.pl>
2022-07-19 12:41:36 +0200

    Page for Exercise Tracker added

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.get("/URLShortenerMicroservice", function (req, res) {
   res.sendFile(__dirname + "/views/URLShortenerMicroservice.html");
 });
 
+app.get("/exerciseTracker", function (req, res) {
+  res.sendFile(__dirname + "/views/exerciseTracker.html");
+});
+
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
